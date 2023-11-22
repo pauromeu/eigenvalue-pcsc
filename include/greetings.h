687df76103a55bc40cbe78c6e2bbe77700d0e058
Pau Romeu <romeu.pau@gmail.com>
2023-11-22 11:01:22 +0100

    add hello world documentation to test doxyfile

--- include/greetings.h
+++ include/greetings.h
@@ -3,6 +3,12 @@
 
 #include <string>
 
+/**
+ * Generate a greeting message.
+ *
+ * @param name The name of the person to greet.
+ * @return A string containing the greeting message.
+ */
 std::string getGreeting(const std::string &name);
 
 #endif // GREETING_H
--- src/greetings.cpp
+++ src/greetings.cpp
@@ -1,5 +1,11 @@
 #include "greetings.h"
 
+/**
+ * Implements the function to generate a greeting message.
+ *
+ * @param name The name of the person to greet.
+ * @return A string containing the greeting message.
+ */
 std::string getGreeting(const std::string &name)
 {
     return "Hello, " + name + "!";
