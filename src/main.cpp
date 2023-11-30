b3d29c5febdaafdeff8da7af6bfbc6946c925e13
Pau Romeu <romeu.pau@gmail.com>
2023-11-30 17:37:45 +0100

    add methods to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include "Greeter.h"
+#include "matrix_readers/MTXMatrixReader.h"
 
 int main()
 {
+    // Ask name to user
     std::string name;
     std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-
+    std::cin >> name;
     Greeter greeter(name);
     std::cout << greeter.sayHello() << std::endl;
 
-    return 0;
+    // Aske user name of matrix to laod
+    std::string matrixName;
+    std::cout << "Enter matrix name: ";
+    std::cin >> matrixName;
+
+    // Load matrix
+    MTXMatrixReader reader = MTXMatrixReader("data/matrix/" + matrixName + ".mtx");
+
+    reader.readMatrix();
+
+    auto matrix = reader.getDenseMatrix();
+
+    // Print matrix
+    std::cout << matrix << std::endl;
 }
