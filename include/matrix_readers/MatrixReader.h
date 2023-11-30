be2e5e3472a892c4895b3466dcb509702727513e
Pau Romeu <romeu.pau@gmail.com>
2023-11-30 18:47:55 +0100

    add documentation

--- include/matrix_readers/MatrixReader.h
+++ include/matrix_readers/MatrixReader.h
@@ -3,15 +3,33 @@
 
 #include <Eigen/Sparse>
 
+/**
+ * @brief Abstract base class for matrix readers.
+ *
+ * Defines the interface for reading matrices from various file formats.
+ */
 class MatrixReader
 {
 public:
     virtual ~MatrixReader() {}
 
+    /**
+     * @brief Reads the matrix from the file.
+     */
     virtual void readMatrix() = 0;
 
+    /**
+     * @brief Retrieve the Eigen sparse matrix.
+     *
+     * @return Eigen::SparseMatrix<double> The matrix in sparse format.
+     */
     virtual Eigen::SparseMatrix<double> getSparseMatrix() const = 0;
 
+    /**
+     * @brief Retrieve the Eigen dense matrix.
+     *
+     * @return Eigen::MatrixXd The matrix in dense format.
+     */
     virtual Eigen::MatrixXd getDenseMatrix() const = 0;
 };
 
