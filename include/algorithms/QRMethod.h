d6074ffd411a3645ceb3662f850521884b1e17be
Pau Romeu <romeu.pau@gmail.com>
2023-12-04 18:24:27 +0100

    add templates

--- include/algorithms/QRMethod.h
+++ include/algorithms/QRMethod.h
@@ -10,20 +10,28 @@ template <typename Scalar>
 class QRMethod : public EigenvalueSolver<Scalar>
 {
 public:
-    QRMethod(int maxIterations = 100, Scalar tol = 1e-6);
+    QRMethod(int maxIterations = 10000000, Scalar tol = 1e-6) : currentIteration(0), maxIterations(maxIterations), tolerance(tol) {}
 
     void setMatrix(const Eigen::MatrixX<Scalar> &matrix) override;
-    void solve() override;
+    void setMaxIterations(int maxIter) override;
+    void setTolerance(Scalar tol) override;
 
 protected:
+    void initialize() override;
     bool hasConverged() const override;
     void performIteration() override;
-    void initialize() override;
+    void obtainResults() override;
 
 private:
     Eigen::MatrixX<Scalar> matrix;
     int currentIteration;
+    int maxIterations;
     Scalar tolerance;
 };
+
+template class QRMethod<double>;
+template class QRMethod<float>;
+template class QRMethod<std::complex<float>>;
+template class QRMethod<std::complex<double>>;
 
 #endif // QRMETHOD_H
