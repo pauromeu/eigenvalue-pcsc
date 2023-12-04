447e60cead5b3cc9dbdb003b7aa419f7c8222d07
Pau Romeu <romeu.pau@gmail.com>
2023-12-04 18:24:16 +0100

    generalize method

--- include/algorithms/EigenvalueSolver.h
+++ include/algorithms/EigenvalueSolver.h
@@ -3,33 +3,42 @@
 
 #include <Eigen/Dense>
 #include <complex>
+#include <type_traits>
 
 template <typename Scalar>
 class EigenvalueSolver
 {
 public:
+    static_assert(std::is_floating_point<Scalar>::value || std::is_same<Scalar, std::complex<float>>::value || std::is_same<Scalar, std::complex<double>>::value,
+                  "Scalar must be a floating point or complex type");
+
     virtual ~EigenvalueSolver() {}
 
     virtual void setMatrix(const Eigen::MatrixX<Scalar> &matrix) = 0;
     virtual void setMaxIterations(int maxIter) = 0;
     virtual void setTolerance(Scalar tol) = 0;
 
-    virtual void solve() = 0;
+    void solve()
+    {
+        initialize();
+        while (!hasConverged())
+        {
+            performIteration();
+        }
+        obtainResults();
+    }
 
-    Eigen::VectorX<Scalar> getEigenvalues() const { return eigenvalues; }
+    Eigen::VectorX<std::complex<double>> getEigenvalues() const { return eigenvalues; }
     Eigen::MatrixX<Scalar> getEigenvectors() const { return eigenvectors; }
 
 protected:
+    virtual void initialize() = 0;
     virtual bool hasConverged() const = 0;
     virtual void performIteration() = 0;
-    virtual void initialize() = 0;
+    virtual void obtainResults() = 0;
 
-private:
-    Eigen::MatrixX<Scalar> matrix;
-    Eigen::VectorX<Scalar> eigenvalues;
+    Eigen::VectorX<std::complex<double>> eigenvalues;
     Eigen::MatrixX<Scalar> eigenvectors;
-    int maxIterations;
-    Scalar tolerance;
 };
 
 #endif // EIGENVALUE_SOLVER_H
