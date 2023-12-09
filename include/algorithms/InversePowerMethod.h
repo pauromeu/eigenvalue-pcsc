8cc04640f038baa267938bf7bfe429167eb7b50b
Tachi67 <haolongli_neko@outlook.com>
2023-12-09 21:00:13 +0100

    add support for inversepowermethod&complex & tests

--- include/algorithms/InversePowerMethod.h
+++ include/algorithms/InversePowerMethod.h
@@ -31,7 +31,15 @@ protected:
         Eigen::Matrix<Scalar, Eigen::Dynamic, 1> vectorizedCurrentVector = this->currentVector;
         Scalar eigenvalue = (matrixInverse * vectorizedCurrentVector).dot(vectorizedCurrentVector) / vectorizedCurrentVector.squaredNorm();
 
-        this->eigenvalues = Eigen::VectorX<Scalar>::Constant(1, 1 / eigenvalue);
+//        this->eigenvalues = Eigen::VectorX<Scalar>::Constant(1, 1 / eigenvalue);
+//        this->eigenvectors = vectorizedCurrentVector;
+
+        if constexpr (std::is_same<Scalar, std::complex<float>>::value || std::is_same<Scalar, std::complex<double>>::value) {
+            Scalar one = Scalar(1.0, 0.0);
+            this->eigenvalues = Eigen::VectorX<Scalar>::Constant(1, one / eigenvalue);
+        } else {
+            this->eigenvalues = Eigen::VectorX<Scalar>::Constant(1, 1 / eigenvalue);
+        }
         this->eigenvectors = vectorizedCurrentVector;
     }
 };
