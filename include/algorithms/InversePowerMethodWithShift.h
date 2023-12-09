ab70a9dee9bade6c4805a650a30ff501ec016a61
Tachi67 <haolongli_neko@outlook.com>
2023-12-09 21:13:27 +0100

    add support for InversePowerMethod&Complex & tests

--- include/algorithms/InversePowerMethodWithShift.h
+++ include/algorithms/InversePowerMethodWithShift.h
@@ -35,7 +35,14 @@ protected:
         Eigen::Matrix<Scalar, Eigen::Dynamic, 1> vectorizedCurrentVector = this->currentVector;
         Scalar eigenvalue = (matrixInverse * vectorizedCurrentVector).dot(vectorizedCurrentVector) / vectorizedCurrentVector.squaredNorm();
 
-        this->eigenvalues = Eigen::VectorX<Scalar>::Constant(1, 1.0 / eigenvalue + shift);
+//        this->eigenvalues = Eigen::VectorX<Scalar>::Constant(1, 1.0 / eigenvalue + shift);
+//        this->eigenvectors = vectorizedCurrentVector;
+        if constexpr (std::is_same<Scalar, std::complex<float>>::value || std::is_same<Scalar, std::complex<double>>::value) {
+            Scalar one = Scalar(1.0, 0.0);
+            this->eigenvalues = Eigen::VectorX<Scalar>::Constant(1, one / eigenvalue + shift);
+        } else {
+            this->eigenvalues = Eigen::VectorX<Scalar>::Constant(1, 1 / eigenvalue + shift);
+        }
         this->eigenvectors = vectorizedCurrentVector;
     }
 
