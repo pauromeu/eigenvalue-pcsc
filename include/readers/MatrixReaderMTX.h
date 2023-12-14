d66cf8a865376cd378fffc85fddf217af421dd3b
Pau Romeu <romeu.pau@gmail.com>
2023-12-14 20:42:45 +0100

    throw coherent exceptions

--- include/readers/MatrixReaderMTX.h
+++ include/readers/MatrixReaderMTX.h
@@ -22,9 +22,11 @@ public:
 
     void printMetadata()
     {
-        std::cout << "======== " + name + " ========" << std::endl;
+        std::cout << "====== Matrix: " + name + " ======" << std::endl;
         std::cout << "Rows: " << rows << ", Cols: " << cols << ", NNZ: " << nnz << std::endl;
-        std::cout << "Is complex: " << isComplex << ", Is symmetric: " << isSymmetric << std::endl;
+        std::cout << std::boolalpha
+                  << "Is complex: " << isComplex
+                  << ", Is symmetric: " << isSymmetric << std::endl;
         std::cout << "=========================" << std::endl;
     }
 
@@ -48,9 +50,19 @@ private:
     void parseFile(const std::string &filename)
     {
         std::ifstream file(filename);
+
+        // Assert file is open
         if (!file.is_open())
         {
-            throw std::runtime_error("Unable to open file: " + filename);
+            throw IOFileException("Failed to open file: " + filename,
+                                  "Make sure the matrix is in the data/matrix/ folder.");
+        }
+
+        // Assert file extension is .mtx
+        if (filename.substr(filename.find_last_of(".") + 1) != "mtx")
+        {
+            throw IOFileException("Invalid file extension: " + filename,
+                                  "Make sure the matrix is in the Matrix Market format (.mtx).");
         }
 
         name = filename.substr(filename.find_last_of("/") + 1, filename.find_last_of(".") - filename.find_last_of("/") - 1);
@@ -72,7 +84,6 @@ private:
         isSymmetric = false;
         if (line.find("complex") != std::string::npos)
         {
-            std::cout << "Found complex" << std::endl;
             isComplex = true;
         }
         if (line.find("symmetric") != std::string::npos)
